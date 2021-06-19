218b1567e258945979313b3d2e86371293e9fa4d
Yisus7u7 <jesuspixel5@gmail.com>
2021-06-19 11:55:02 -0500

    fix bugs

--- build.py
+++ build.py
@@ -13,7 +13,7 @@ from pkg_info import *
 pkg_data = open("./termux_pkg/DEBIAN/control", "r+")
 pkg_data.write(f"Package: {TERMUX_PKG_NAME}\n")
 pkg_data.write(f"Version: {TERMUX_PKG_VERSION}\n")
-pkg_data.write(f"Architecture: {TERMUX_PKG_ARCH}")
+pkg_data.write(f"Architecture: {TERMUX_PKG_ARCH}\n")
 pkg_data.write(f"Maintainer: {TERMUX_PKG_MAINTAINER}\n")
 pkg_data.write(f"Installed-Size: {TERMUX_PKG_SIZE}\n")
 pkg_data.write(f"Depends: {TERMUX_PKG_DEPENDS}\n")
